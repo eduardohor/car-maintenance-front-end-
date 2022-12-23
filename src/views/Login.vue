5ed972b44f91886cd53c4ababc0768323a1c25e6
eduado <eduardo.hor@outlook.com>
2022-12-23 13:01:39 -0300

    :heavy_plus_sign: feat: creating login screen components

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,23 +2,18 @@
 
   <main class="form-signin w-100 m-auto">
     <form>
-      <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+      <h1 class="h3 mb-3 fw-normal">Por favor, faça o login</h1>
 
       <div class="form-floating">
         <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-        <label for="floatingInput">Email address</label>
+        <label for="floatingInput">Email</label>
       </div>
       <div class="form-floating">
-        <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-        <label for="floatingPassword">Password</label>
+        <input type="password" class="form-control" id="floatingPassword" placeholder="Senha">
+        <label for="floatingPassword">Senha</label>
       </div>
 
-      <div class="checkbox mb-3">
-        <label>
-          <input type="checkbox" value="remember-me"> Remember me
-        </label>
-      </div>
-      <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+      <button class="w-100 btn btn-lg btn-primary" type="submit">Entrar</button>
     </form>
   </main>
 
