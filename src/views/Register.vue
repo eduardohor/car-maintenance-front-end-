4dbe41f2ebf30513c6f4d6a37d6cdaaade5e26b4
eduado <eduardo.hor@outlook.com>
2022-12-23 13:05:52 -0300

    :heavy_plus_sign: feat: creating register screen components

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -2,14 +2,18 @@
 
   <main class="form-signin w-100 m-auto">
     <form>
-      <h1 class="h3 mb-3 fw-normal">Por favor, faça o login</h1>
+      <h1 class="h3 mb-3 fw-normal">Por favor, cadastre-se</h1>
 
       <div class="form-floating mb-2">
-        <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+        <input type="text" class="form-control"  placeholder="Nome">
+        <label for="floatingInput">Nome</label>
+      </div>
+      <div class="form-floating mb-2">
+        <input type="email" class="form-control" placeholder="name@example.com">
         <label for="floatingInput">Email</label>
       </div>
       <div class="form-floating mb-2">
-        <input type="password" class="form-control" id="floatingPassword" placeholder="Senha">
+        <input type="password" class="form-control"  placeholder="Senha">
         <label for="floatingPassword">Senha</label>
       </div>
 
